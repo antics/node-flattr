150bb8bb5701284a0424cd956eb8c7314547b20f
Michel Legnered <michel.legnered@gmail.com>
2012-01-24 13:26:21 +0100

    Flattrs done

--- examples/test.js
+++ examples/test.js
@@ -1,12 +1,46 @@
 var flattrs = require('../flattr').flattrs;
+var things = require('../flattr').things;
 
 
+/*
 flattrs.list('Jerec', 3, 1, function (data, headers) {
 	console.log(data, headers);
 });
+*/
 
 /*
 flattrs.list_auth('', '', function (data, headers) {
 	console.log(data, headers);
 });
+*/
+
+/* TODO: Test when authenticated
+flattrs.thing(313733, function (data) {
+	console.log(data);
+});
+*/
+
+// TODO: Test when authenticated
+flattrs.url('http://inventoria.se', 'user', function (data) {
+	console.log(data);
+});
+
+
+/*
+things.list('Jerec', function (data, headers) {
+	console.log(data, headers);
+});
+*/
+
+/*
+things.list_auth(function (data, headers) {
+	console.log(data, headers);
+});
+*/
+
+/*
+things.get(423405, function (data, head) {
+	console.log(data, head);
+});
+
 */
--- flattr.js
+++ flattr.js
@@ -28,10 +28,10 @@ function Flattrs () {
 	// page ( Optional ) - Page of results to retreive
 	// callback - callback function
 	//
-	self.list = function () {
+	self.list = function (user) {
 
+		// Params
 		var
-		user  = arguments[0],
 		count = (typeof arguments[1] === 'number') ? arguments[1] : '',
 		page  = (typeof arguments[2] === 'number') ? arguments[2] : '',
 		callback = arguments[arguments.length-1];
@@ -71,9 +71,93 @@ function Flattrs () {
 			callback(data, headers)
 		});
 	};
+
+	// Flattr a thing
+	//
+	// Arguments
+	// id - id of thing to flattr
+	// callback - callback function
+	//
+	self.thing = function (id, callback) {
+		var httpsopts = {
+			hostname: o.host,
+			path: o.endpoint+'/things/'+id+'/flattr',
+			method: 'POST'
+		};
+
+		make_request(httpsopts, function (data, headers) {
+			callback(data, headers)
+		});
+		
+	}
+
+	// Flattr an URL
+	//
+	// Arguments
+	// url - URL of th thing
+	// user - flattr user name
+	// params (optional) -  Parameters, see below
+	// callback - callback function
+	//
+	// Optional parameters
+	// title (Optional) - Title of your your thing.
+	// description (Optional) - Description for your thing.
+	// language (Optional) - Language of your page, use one of the available languages.
+	// tags (Optional) - Tags you want your post to be tagged with. Multiple tags are separated with ,
+	// hidden (Optional) - If you want to hide the things from public listings on flattr.com set hidden to 1.
+	// category (Optional) - Category from the list of categories found here.
+	//	
+	self.url = function (url, user) {
+
+		var query_str = '';
+		
+		// arguments[2] == Optional parameters
+		if (typeof arguments[2] === 'object') {
+			var params = arguments[2];
+			
+			for (key in params)
+				query_str += '&'+key+'='+params[key];
+		}
+
+		if (arguments.length > 2)
+			var callback = arguments[arguments.length-1];
+
+		var
+		httpsopts = {
+			hostname: o.host,
+			path: o.endpoint+'/flattr',
+			method: 'POST',
+		},
+		data = {
+			"url": 'http://flattr.com/submit/auto?user_id='+user+'&url='+
+				encodeURIComponent(/^(http|https):\/\//.test(url) ? url : 'http://'+url)+query_str
+		};
+		
+		make_request(httpsopts, data, function (data, headers) {
+			callback(data, headers)
+		});
+		
+	}
 }
 
-function make_request (httpsopts, callback) {
+// Https request helper function
+//
+// Arguments
+// httpsopts - options for the https request
+// reqdata (optional) - data to be sent to server
+// callback - callback function
+//
+function make_request (httpsopts) {
+
+	var
+	reqdata  = (typeof arguments[1] === 'object') ? JSON.stringify(arguments[1]) : '',
+	callback = arguments[arguments.length-1];
+
+	httpsopts.headers = {
+		"Content-Length": reqdata.length,
+		"Content-Type": 'application/json'
+	};
+	
 	var req = https.request(httpsopts, function (res) {
 		var data = '';
 
@@ -92,5 +176,5 @@ function make_request (httpsopts, callback) {
 		console.log(err);
 	});
 
-	req.end();
+	req.end(reqdata);
 }
